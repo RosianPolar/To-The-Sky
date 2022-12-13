8490780933eff3fae8116c544cb894d0b7250ed9
tomkwlee <81406909+tomkwlee@users.noreply.github.com>
2022-12-13 02:02:18 -0800

    sound effect scripts

---
+++ Assets/clickSound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickSound : MonoBehaviour
+{
+
+    public AudioSource soundPlayer;
+
+    public void playSoundEffect() {
+        soundPlayer.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
